9a59c9a3ee45a501217b489b721bf12e82e9dee1
DDVTomu <phan031099@gmail.com>
2023-06-21 00:27:22 +0700

    init

---
+++ src/components/body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Item from "./Body/item";
+export default function Body() {
+  return (
+    <>
+      {" "}
+      <section class="pt-4">
+        <div class="container px-lg-5">
+          <Item />
+        </div>
+      </section>
+    </>
+  );
+}
